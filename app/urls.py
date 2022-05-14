ad973bf54e0a08f21d04333073f2beb3b91bd749
demirug <pm.antondolia@gmail.com>
2022-05-14 18:11:48 +0300

    Register auth urls

--- app/urls.py
+++ app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from apps.account.urls import urlpatterns as auth_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += auth_urls
