4a923d69d3b9350b0a48e4fb165f69f603264253
demirug <pm.antondolia@gmail.com>
2022-05-20 10:52:17 +0300

    Created Anime's list view/contoroller

--- apps/movie/views.py
+++ apps/movie/views.py
@@ -1,1 +1,15 @@
-from django.shortcuts import render
+from django.db.models import Count
+from django.views.generic import ListView
+
+from apps.movie.models import Anime
+
+
+class AnimeListView(ListView):
+    """Controller to display List of Anime's"""
+
+    model = Anime
+    template_name = "movie/list.jinja"
+
+    def get_queryset(self, ):
+        """Display anime's with available seasons"""
+        return Anime.objects.annotate(seasons_cnt=Count('seasons')).filter(seasons_cnt__gt=0)
