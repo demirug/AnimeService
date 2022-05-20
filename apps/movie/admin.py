136850b99fe079b900bd69b60527613b6cde2e05
demirug <pm.antondolia@gmail.com>
2022-05-20 10:51:11 +0300

    Registered movie models in admin

--- apps/movie/admin.py
+++ apps/movie/admin.py
@@ -1,1 +1,20 @@
 from django.contrib import admin
+
+from apps.movie.models import EpisodeFile, Episode, Quality, Season, Anime
+
+
+class EpisodeFileInline(admin.TabularInline):
+    model = EpisodeFile
+    extra = 1
+
+
+@admin.register(Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    inlines = [
+        EpisodeFileInline,
+    ]
+
+
+admin.site.register(Quality)
+admin.site.register(Season)
+admin.site.register(Anime)
