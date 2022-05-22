9dd1ad129a4b36ab58c5e8a2fd37c0e00017d02c
demirug <pm.antondolia@gmail.com>
2022-05-22 12:41:10 +0300

    Installed django-debug-toolbar

--- app/settings.py
+++ app/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'debug_toolbar',
+
     'shared',
     'apps.account',
     'apps.movie'
@@ -58,6 +60,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = 'app.urls'
--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from apps.account.urls import urlpatterns as auth_urls
 from apps.movie.urls import urlpatterns as movie_urls
@@ -15,3 +15,4 @@ urlpatterns += movie_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
