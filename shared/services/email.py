8e27d4839bc3a48ce5faa12d08f65d2d9fb6f16d
demirug <pm.antondolia@gmail.com>
2022-05-27 18:48:11 +0300

    Updated email service to work with celery

--- shared/services/email.py
+++ shared/services/email.py
@@ -1,13 +1,17 @@
-from django.conf import settings
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-from django.core import mail
+
+from shared.tasks import send_task_mail
 
 
 def send_email(email, subject, template, context=None):
     """Send email to user using templates"""
+
     html_message = render_to_string(template, context)
     plain_message = strip_tags(html_message)
-    mail.send_mail(subject, plain_message, settings.DEFAULT_FROM_EMAIL, [email], html_message=html_message,
-                   fail_silently=True)
-    #TODO send email by celery task
+
+    if isinstance(email, str):
+        email = [email]
+
+    send_task_mail(subject, html_message, plain_message, email)
+
