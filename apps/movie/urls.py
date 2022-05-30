72a27043a8d222df991d1839b2fc80017dd11585
demirug <pm.antondolia@gmail.com>
2022-05-30 15:37:18 +0300

    Registered ReviewCreateUpdateView path

--- apps/movie/urls.py
+++ apps/movie/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 
     path("<slug:slug>/", AnimeDetailView.as_view(), name="detail"),
     path("<slug:slug>/<int:season>/", AnimeDetailView.as_view(), name="detail"),
+    path("review/<slug:slug>/<int:season>/", ReviewCreateUpdateView.as_view(), name="review"),
 ]
