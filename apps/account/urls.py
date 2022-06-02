a2b86b79142599e95c0a9e591935f522207c5633
demirug <pm.antondolia@gmail.com>
2022-06-02 20:28:44 +0300

    Added missed import

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 import django.contrib.auth.views as auth_views
-from apps.account.views import ValidateEmailView
+from apps.account.views import ValidateEmailView, AccountRegisterView
 
 urlpatterns = [
     path("verify/<str:username>/<str:token>/", ValidateEmailView.as_view(), name="verify"),
