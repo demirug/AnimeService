4faae07be22dd59d884f5d2ccba5d265c91d06c5
demirug <pm.antondolia@gmail.com>
2022-06-03 21:44:18 +0300

    Created password change

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -4,6 +4,12 @@ from apps.account.views import ValidateEmailView, AccountRegisterView
 
 urlpatterns = [
     path("verify/<str:username>/<str:token>/", ValidateEmailView.as_view(), name="verify"),
+
+    path("changepass/", auth_views.PasswordChangeView.as_view(template_name="account/password_change.jinja"),
+         name='password_change'),
+    path("changepass/done/", auth_views.PasswordChangeDoneView.as_view(template_name="account/password_change_done.jinja"),
+         name='password_change_done'),
+
     path("login/", auth_views.LoginView.as_view(template_name="account/login.jinja"), name="login"),
     path("register/", AccountRegisterView.as_view(), name="register"),
     path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
