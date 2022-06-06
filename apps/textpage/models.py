10412ccf25d799e582f47cbdc3c79382d29c4131
demirug <pm.antondolia@gmail.com>
2022-06-06 16:33:27 +0300

    Renamed field on TextPage model

--- apps/textpage/models.py
+++ apps/textpage/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
@@ -7,7 +8,7 @@ class TextPage(models.Model):
     name = models.CharField(verbose_name=_("Name"), max_length=150)
     slug = models.SlugField("Slug", unique=True)
     content = models.TextField(verbose_name=_("Content"))
-    draft = models.BooleanField(verbose_name=_("Состояние"), default=False)
+    draft = models.BooleanField(verbose_name=_("Draft"), default=False)
 
     created_at = models.DateTimeField(_("Creation date"), auto_now_add=True, editable=False)
 
@@ -17,3 +18,6 @@ class TextPage(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("textpage", kwargs={"slug": self.slug})
