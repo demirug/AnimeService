bb7da58f0bb5888958d5e2307fc1aad754f71c74
demirug <pm.antondolia@gmail.com>
2022-06-06 15:00:17 +0300

    Registered subscribe url

--- apps/movie/urls.py
+++ apps/movie/urls.py
@@ -4,7 +4,8 @@ from apps.movie.views import *
 urlpatterns = [
     path("", AnimeListView.as_view(), name="home"),
 
-    path("<slug:slug>/", AnimeDetailView.as_view(), name="detail"),
-    path("<slug:slug>/<int:season>/", AnimeDetailView.as_view(), name="detail"),
+    path("view/<slug:slug>/", AnimeDetailView.as_view(), name="detail"),
+    path("view/<slug:slug>/<int:season>/", AnimeDetailView.as_view(), name="detail"),
     path("review/<slug:slug>/<int:season>/", ReviewCreateUpdateView.as_view(), name="review"),
+    path("subscribe/<slug:slug>/", SubscribeView.as_view(), name="subscribe"),
 ]
