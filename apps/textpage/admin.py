e5a0edc60088039ec171b5503d2d9ce70c9c67fd
demirug <pm.antondolia@gmail.com>
2022-06-06 16:34:04 +0300

    Added view link to TextPage admin

--- apps/textpage/admin.py
+++ apps/textpage/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from django.utils.html import format_html
 
 from apps.textpage.forms import TextPageAdminForm
 from apps.textpage.models import TextPage
@@ -10,3 +11,8 @@ class TextPageAdmin(ModelAdmin):
     form = TextPageAdminForm
     search_fields = ('name',)
     list_display = ['name', 'draft', 'created_at']
+    readonly_fields = ['page_link']
+
+    def page_link(self, instance):
+        """Displays redirect button to season"""
+        return format_html(f'<a target="_blank" href="{instance.get_absolute_url()}">View</a>')
