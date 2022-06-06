54fa6ca214377b1a19a8b36c289c10ada1a49b05
demirug <pm.antondolia@gmail.com>
2022-06-06 16:26:25 +0300

    Register textpage in admin

--- apps/textpage/admin.py
+++ apps/textpage/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from apps.textpage.forms import TextPageAdminForm
+from apps.textpage.models import TextPage
+
+
+@admin.register(TextPage)
+class TextPageAdmin(ModelAdmin):
+    form = TextPageAdminForm
+    search_fields = ('name',)
+    list_display = ['name', 'draft', 'created_at']
