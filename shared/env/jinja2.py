9e2dfb70126086c936cab04b6b703ed08ae1a229
demirug <pm.antondolia@gmail.com>
2022-06-06 15:35:45 +0300

    Register menu template tags in environment

--- shared/env/jinja2.py
+++ shared/env/jinja2.py
@@ -1,8 +1,13 @@
 from jinja2 import Environment
 
+from shared.template_tags.menu import header_objects, footer_objects
+
 
 def environment(**options):
     options['cache_size'] = 0
     env = Environment(**options)
-
+    env.globals.update({
+        'header_objects': header_objects,
+        'footer_objects': footer_objects,
+    })
     return env
