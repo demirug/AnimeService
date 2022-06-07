2515eede1c99c3a37a93c0ee1db73b6a39903167
demirug <pm.antondolia@gmail.com>
2022-06-07 14:52:11 +0300

    Created name/tag filter for Anime

---
+++ apps/movie/filters.py
@@ -0,0 +1,29 @@
+import django_filters
+
+from apps.movie.models import Anime, Tag
+
+
+class AnimeFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(label='Search', method='name_tag_filter')
+
+    def name_tag_filter(self, queryset, name, value: str):
+        """Filter """
+        data = value.split(' ')
+
+        tags = [tag.lower()[1:] for tag in data if tag.startswith('#')]
+        search = ' '.join([text for text in data if not text.startswith('#')])
+
+        if tags:
+            tags_objects = Tag.objects.filter(name__in=tags)
+            # If given tag not found return empty results
+            if tags_objects.count() != len(tags):
+                return Anime.objects.none()
+
+            anime_pk = tags_objects.filter(anime__name__icontains=search).values_list('anime__pk', flat=True)
+            return queryset.filter(pk__in=anime_pk)
+
+        return queryset.filter(name__icontains=search)
+
+    class Meta:
+        model = Anime
+        fields = ['name']
