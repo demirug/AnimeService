374f0a2c4b785fee15dd556b8027b8a5144f62e4
demirug <pm.antondolia@gmail.com>
2022-06-12 18:02:11 +0300

    Created AccountReset views

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth import get_user_model, update_session_auth_hash
+from django.contrib.auth import get_user_model, update_session_auth_hash, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.tokens import default_token_generator as generator
+from django.contrib.sites.models import Site
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
@@ -14,7 +15,9 @@ from django.views.generic import TemplateView, RedirectView, ListView, FormView
 from django_jinja.views.generic import CreateView, UpdateView
 
 from shared.mixins.breadcrumbs import BreadCrumbsMixin
-from .forms import UserCreationForm, AccountUpdateForm, UserPasswordChangeForm
+from shared.services.email import send_email
+from .forms import UserCreationForm, AccountUpdateForm, UserPasswordChangeForm, AccountResetForm, \
+    AccountResetConfirmForm
 from ..movie.models import Subscribe
 
 
@@ -140,3 +143,60 @@ class AccountEmailChangeView(LoginRequiredMixin, RedirectView):
         user.save()
 
         return super().get(request, *args, **kwargs)
+
+
+class AccountResetView(FormView):
+    """Account reset password view"""
+    form_class = AccountResetForm
+    template_name = "account/reset.jinja"
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect(reverse("home"))
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        """Send email with generated reset token"""
+        _email = form.cleaned_data['email']
+        user = get_user_model().objects.filter(email=_email).first()
+        if user:
+            token = generator.make_token(user)
+
+            url = '{domain}{path}'.format(domain=Site.objects.get_current().domain,
+                                          path=reverse("reset_confirm", kwargs={"email": _email, "token": token}))
+
+            send_email(_email, _("Reset account"), "email/reset_account.jinja", context={"url": url})
+
+        return render(self.request, "account/reset_sent.jinja", context={})
+
+
+class AccountResetConfirmView(FormView):
+    """Confirm account reset password"""
+    form_class = AccountResetConfirmForm
+    template_name = "account/reset_confirm.jinja"
+
+    def dispatch(self, request, *args, **kwargs):
+        """If user or token incorrect 404"""
+        if self.request.user.is_authenticated:
+            return redirect(reverse("home"))
+        self.user = get_object_or_404(get_user_model(), email=kwargs['email'])
+
+        if not generator.check_token(self.user, kwargs['token']):
+            raise Http404(_("Incorrect token"))
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({"user": self.user})
+        return kwargs
+
+    def form_valid(self, form):
+        """Set user password from form and login"""
+        self.user.set_password(form.cleaned_data["password1"])
+        self.user.save()
+
+        login(self.request, self.user)
+
+        messages.success(self.request, _("Password changed success"))
+        return redirect("profile")
