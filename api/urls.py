8c185d90213b9e456d065783905892c45fd2fa5a
demirug <pm.antondolia@gmail.com>
2022-06-12 23:56:13 +0300

    Created api for user info

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 
+from api.v1.account.views import CurrentUserView
 from api.v1.movie.views import EpisodeRetrieveAPIView, AnimeRetrieveApiView
 
 urlpatterns = [
     path('v1/', include([
         path('episode/<slug:slug>/<int:season>/<int:episode>/', EpisodeRetrieveAPIView.as_view(), name='episode'),
-        path('anime/<slug:slug>/', AnimeRetrieveApiView.as_view(), name='anime')
+        path('anime/<slug:slug>/', AnimeRetrieveApiView.as_view(), name='anime'),
+        path('user/', CurrentUserView.as_view(), name="user"),
     ])),
     path('api-auth/', include('rest_framework.urls')),
 ]
---
+++ api/v1/account/views.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from api.v1.account.serializers import UserSerializer
+
+
+class CurrentUserView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
