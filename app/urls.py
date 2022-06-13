3e37d638662938d2258fdd4ae33f5e3b0e84747a
demirug <pm.antondolia@gmail.com>
2022-06-13 23:07:49 +0300

    Registered news url paths in urls

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.news.views import *
 from apps.textpage.views import TextPageDetailView
 
 urlpatterns = [
     path('api/', include(('api.urls', 'api'))),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
+
+    path("", NewsListView.as_view(), name="home"),
+    path("news/<slug:slug>/", NewsDetailView.as_view(), name="news"),
+
     path("anime/", include(("apps.movie.urls", "movie"))),
     path("", include(("apps.account.urls", "account"))),
     path("page/<slug:slug>/", TextPageDetailView.as_view(), name="textpage"),
