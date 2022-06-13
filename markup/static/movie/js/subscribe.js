45508895b82a12ce4455fbf1309df64dde256efa
demirug <pm.antondolia@gmail.com>
2022-06-13 14:03:06 +0300

    Removed useless method

--- markup/static/movie/js/subscribe.js
+++ markup/static/movie/js/subscribe.js
@@ -29,9 +29,3 @@ function post_rqs(url) {
     request.send();
     return request.responseText
 }
-
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-}
