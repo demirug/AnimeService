8f1da1a76218401a32e86ac1f186e0191a14cbfc
demirug <pm.antondolia@gmail.com>
2022-06-14 15:05:13 +0300

    Added displaying recommendation on AnimeDetailView

---
+++ markup/static/movie/js/recommended-movie.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    // 50% chance to show recommended anime
+    if(Math.floor(Math.random() * 2) === 0) return;
+
+    let episode_selector = document.getElementById("episode-selector");
+
+    //If episode selector not exists return
+    if (typeof(episode_selector) == 'undefined' && episode_selector == null) return;
+
+    let slug = episode_selector.getAttribute("anime");
+
+    let answer = JSON.parse(request("/api/v1/anime/random/" + slug))
+
+    // If detail in response -> recommendation not found (404)
+    if(answer.detail) return;
+
+    new MainModal().setTitle("Recommended anime")
+        .addToBody("<h5>" + answer.name + "</h5>")
+        .addToBody("<img style='max-width: 300px' src='" + answer.poster + "'/>")
+        .addToFooter("<a class='btn btn-primary' href='" + answer.url + "' target='_blank'>View</a>")
+        .addFooterCloseButton()
+        .show();
+});
