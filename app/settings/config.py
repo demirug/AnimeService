103fa465b256573de76b1bf476244b6262fcb6e6
demirug <pm.antondolia@gmail.com>
2022-06-17 21:06:30 +0300

    Set DRF settings

--- app/settings/config.py
+++ app/settings/config.py
@@ -185,6 +185,14 @@ USE_TZ = True
 
 SITE_ID = 1
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
