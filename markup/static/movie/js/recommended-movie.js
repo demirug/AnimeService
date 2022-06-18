53dbd39cf0e694f7020f5a017a275611d20d04e0
demirug <pm.antondolia@gmail.com>
2022-06-18 13:07:40 +0300

    Changed api path

--- markup/static/movie/js/recommended-movie.js
+++ markup/static/movie/js/recommended-movie.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     let slug = episode_selector.getAttribute("anime");
 
-    let answer = JSON.parse(request("/api/v1/anime/random/" + slug))
+    let answer = JSON.parse(request("/api/v1/movie/random/" + slug))
 
     // If detail in response -> recommendation not found (404)
     if(answer.detail) return;
