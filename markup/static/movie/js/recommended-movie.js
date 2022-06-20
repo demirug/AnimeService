2619a55f862c455af3500a6a04bdc93c3874755c
demirug <pm.antondolia@gmail.com>
2022-06-20 23:22:37 +0300

    Remade recommended-movie script to Jquery, added checking response status code

--- markup/static/movie/js/recommended-movie.js
+++ markup/static/movie/js/recommended-movie.js
@@ -1,24 +1,18 @@
-document.addEventListener('DOMContentLoaded', function(){
+$(document).ready(function () {
 
     // 50% chance to show recommended anime
     if(Math.floor(Math.random() * 2) === 0) return;
 
-    let episode_selector = document.getElementById("episode-selector");
+    let answer = request("/api/v1/movie/random/" + anime_slug + "/")
 
-    //If episode selector not exists return
-    if (typeof(episode_selector) == 'undefined' && episode_selector == null) return;
+    if(answer.status === 404) return;
 
-    let slug = episode_selector.getAttribute("anime");
-
-    let answer = JSON.parse(request("/api/v1/movie/random/" + slug).responseText)
-
-    // If detail in response -> recommendation not found (404)
-    if(answer.detail) return;
+    let data = JSON.parse(answer.responseText)
 
     new MainModal().setTitle("Recommended anime")
-        .addToBody("<h5>" + answer.name + "</h5>")
-        .addToBody("<img style='max-width: 300px' src='" + answer.poster + "'/>")
-        .addToFooter("<a class='btn btn-primary' href='" + answer.url + "' target='_blank'>View</a>")
+        .addToBody("<h5>" + data.name + "</h5>")
+        .addToBody("<img style='max-width: 300px' src='" + data.poster + "'/>")
+        .addToFooter("<a class='btn btn-primary' href='/anime/" + data.slug + "' target='_blank'>View</a>")
         .addFooterCloseButton()
         .show();
 });
