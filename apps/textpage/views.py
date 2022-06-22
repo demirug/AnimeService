e5b6a615c82fbe4e51e5c8fbfdf576e97bf03fd9
demirug <pm.antondolia@gmail.com>
2022-06-22 14:31:14 +0300

    Added breadcrumb to textpage

--- apps/textpage/views.py
+++ apps/textpage/views.py
@@ -1,11 +1,17 @@
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 from django_jinja.views.generic import DetailView
 from apps.textpage.models import TextPage
+from shared.mixins.breadcrumbs import BreadCrumbsMixin
 
 
-class TextPageDetailView(DetailView):
+class TextPageDetailView(BreadCrumbsMixin, DetailView):
     """TextPage detail view"""
     model = TextPage
     template_name = "textpage/page.jinja"
+
+    def get_breadcrumbs(self):
+        return [(_("Home"), reverse("home")), (self.object.name,)]
 
     def get_queryset(self):
         return TextPage.objects.filter(draft=False)
