4fdfe65c3edd11d2e4fece17c61bc69c061998b8
demirug <pm.antondolia@gmail.com>
2022-06-23 16:53:13 +0300

    Created AnswerQuestion view

--- apps/helper/views.py
+++ apps/helper/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from apps.helper.forms import AuthorizeQuestionForm, UnAuthorizeQuestionForm
+from apps.helper.models import DefaultAnswer, Question
+from shared.mixins.breadcrumbs import BreadCrumbsMixin
+
+
+class AnswersQuestionsView(BreadCrumbsMixin, CreateView):
+    """ListView for DefaultAnswer with creating Questions objects"""
+    model = Question
+    template_name = "helper/home.jinja"
+
+    def get_breadcrumbs(self):
+        return [(_("Home"), reverse("home")), (_("Questions"),)]
+
+    def get_form_class(self):
+        """Return form depending on player authorization status"""
+        if self.request.user.is_authenticated:
+            return AuthorizeQuestionForm
+        else:
+            return UnAuthorizeQuestionForm
+
+    def form_valid(self, form):
+        question = form.save(commit=False)
+        if type(form) == AuthorizeQuestionForm:
+            question.email = self.request.user.email
+        question.save()
+
+        messages.success(self.request, _("Your question has been sent. Check your email for a reply"))
+        return redirect("helper")
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['object_list'] = DefaultAnswer.objects.all()
+        return context
