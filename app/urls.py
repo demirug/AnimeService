1904fa227955b4d15a8bd416b6fd2049ea8e85c5
demirug <pm.antondolia@gmail.com>
2022-06-23 16:53:30 +0300

    Registered helper path

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.helper.views import AnswersQuestionsView
 from apps.news.views import *
 from apps.textpage.views import TextPageDetailView
 
@@ -14,6 +15,7 @@ urlpatterns = [
     path("", NewsListView.as_view(), name="home"),
     path("news/<slug:slug>/", NewsDetailView.as_view(), name="news"),
 
+    path("helper/", AnswersQuestionsView.as_view(), name="helper"),
     path("anime/", include(("apps.movie.urls", "movie"))),
     path("", include(("apps.account.urls", "account"))),
     path("page/<slug:slug>/", TextPageDetailView.as_view(), name="textpage"),
