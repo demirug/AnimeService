58c4a339fdd7a0d8b391231dc8ce4683993e513e
demirug <pm.antondolia@gmail.com>
2022-06-24 15:08:38 +0300

    Added translation properties

--- app/settings/config.py
+++ app/settings/config.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from gettext import gettext
 from pathlib import Path
 import os
 
@@ -173,13 +173,26 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+
+LANGUAGES = (
+    ('en', gettext('English')),
+    ('uk', gettext('Українськa')),
+)
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
+MODELTRANSLATION_DEFAULT_LANGUAGE = 'en'
+MODELTRANSLATION_LANGUAGES = ('en', 'uk')
 
 
 # Static files (CSS, JavaScript, Images)
