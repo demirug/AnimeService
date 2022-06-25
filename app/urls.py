00047fc95d1033300bcd79b60982358b5322e6bb
demirug <pm.antondolia@gmail.com>
2022-06-25 18:24:16 +0300

    Registered roseta path

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from apps.helper.views import FeedbackFAQView
 from apps.news.views import *
@@ -12,6 +13,8 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
+    re_path(r'^rosetta/', include('rosetta.urls'))
+]
 
     path("", NewsListView.as_view(), name="home"),
     path("news/<slug:slug>/", NewsDetailView.as_view(), name="news"),
@@ -20,7 +23,7 @@ urlpatterns = [
     path("anime/", include(("apps.movie.urls", "movie"))),
     path("", include(("apps.account.urls", "account"))),
     path("page/<slug:slug>/", TextPageDetailView.as_view(), name="textpage"),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
