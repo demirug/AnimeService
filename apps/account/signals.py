03793276f9d5975f61c496a316ec53c49950d31c
demirug <pm.antondolia@gmail.com>
2022-06-25 09:03:59 +0300

    replace site name by variable

--- apps/account/signals.py
+++ apps/account/signals.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
@@ -11,6 +12,6 @@ from shared.services.email import send_email
 def account_handler(sender, instance: User, **kwargs):
     """Sending welcome email on user register with token to verify account"""
     if kwargs.get("created", False) and not instance.is_active:
-        send_email(instance.email, _("Welcome to AnimeService"), "email/registered.jinja",
+        send_email(instance.email, _("Welcome to %s") % settings.SITE_NAME, "email/registered.jinja",
                    context={"name": instance.username, "token": generator.make_token(instance)})
 
