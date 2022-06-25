545bd4a0a6b8d2f772c6747e14efa716c5c1053b
demirug <pm.antondolia@gmail.com>
2022-06-25 15:10:31 +0300

    Added logic for Anime content type field

--- apps/movie/views.py
+++ apps/movie/views.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _, get_language
 from django_filters.views import FilterView
 from django_jinja.views.generic import DetailView
 
+from apps.movie.constants import AnimeType
 from apps.movie.filters import AnimeFilter
 from apps.movie.forms import ReviewForm
 from apps.movie.models import Anime, Season, Subscribe, MovieSettings
@@ -57,22 +58,27 @@ class AnimeDetailView(BreadCrumbsMixin, DetailView):
 
         anime: Anime = self.object
 
-        if "season" in self.kwargs:
-            context['season'] = get_object_or_404(Season, anime=anime, number=int(self.kwargs['season']))
+        if anime.type is AnimeType.FILM:
+            context['season'] = anime.seasons.first()
         else:
-            context['season'] = anime.seasons.order_by('number').first()
+            if "season" in self.kwargs:
+                context['season'] = get_object_or_404(Season, anime=anime, number=int(self.kwargs['season']))
+            else:
+                context['season'] = anime.seasons.order_by('number').first()
 
-        context['season_list'] = anime.seasons.values_list('number', flat=True)
-        context['episode_list'] = context['season'].episodes.values('number', 'pk')
+            context['season_list'] = anime.seasons.values_list('number', flat=True)
+            context['episode_list'] = context['season'].episodes.values('number', 'pk')
 
         context['reviews'] = context['season'].reviews.select_related("user").order_by("-datetime")
 
         context["settings"] = MovieSettings.get_solo()
+        context["film_type"] = anime.type
 
         if self.request.user.is_authenticated:
             context['form'] = ReviewForm()
 
-            context['subscribe'] = Subscribe.objects.filter(anime=anime, user=self.request.user).exists()
+            if anime.type is AnimeType.SERIAL:
+                context['subscribe'] = Subscribe.objects.filter(anime=anime, user=self.request.user).exists()
 
             rating = self.request.user.ratings.filter(anime=anime).first()
             if rating:
