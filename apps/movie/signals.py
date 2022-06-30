c77912bb3e73b5d9db2dfdf611566a4f277cf018
demirug <pm.antondolia@gmail.com>
2022-07-01 00:26:47 +0300

    Replaced email context template by settings value

--- apps/movie/signals.py
+++ apps/movie/signals.py
@@ -1,11 +1,11 @@
 from django.contrib.sites.models import Site
-from django.db.models import Sum, Avg
+from django.db.models import Avg
 from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
-from django.utils.translation import gettext_lazy as _
 
 from shared.services.email import send_email
-from .models import Tag, Episode, Rating, Anime
+from shared.services.translation import get_field_data_by_user_lang
+from .models import Tag, Episode, Rating, Anime, MovieSettings
 
 
 @receiver(pre_delete, sender=Tag)
@@ -23,16 +23,23 @@ def episode_handler(sender, instance: Episode, **kwargs):
     """On new Episode send email to subscribers"""
     if kwargs.get("created", False):
         anime = instance.season.anime
-        subscribers = list(anime.subscribes.values_list("user__email", flat=True))
-        if len(subscribers) == 0:
-            return
+        subscribers = anime.subscribes.all()
 
-        send_email(subscribers, _("New Episode %s #%s") % (anime.name, instance.number), "email/new_episode.jinja",
-                   context={"url": "{domain}{url}".format(domain=Site.objects.get_current().domain,
-                                                          url=instance.get_absolute_url()),
-                            "episode": instance,
-                            "anime": anime,
-                            })
+        if subscribers.count() > 0:
+            settings: MovieSettings = MovieSettings.get_solo()
+            url_path = "{domain}{url}".format(domain=Site.objects.get_current().domain, url=instance.get_absolute_url())
+
+            for subscriber in subscribers:
+                # If user language like anime language
+                if subscriber.user.lang == anime.lang:
+                    title = get_field_data_by_user_lang(settings, subscriber.user, "new_episode_email_title").format(name=anime.name,
+                                                                                                                number=instance.number)
+
+                    context = get_field_data_by_user_lang(settings, subscriber.user, "new_episode_email").format(name=anime.name,
+                                                                                                            number=instance.number,
+                                                                                                            url=url_path)
+
+                    send_email(subscriber.user.email, title, context)
 
 
 @receiver(post_save, sender=Rating)
