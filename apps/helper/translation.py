2af0be6fd048c4777e70cae3426804d527ca97f2
demirug <pm.antondolia@gmail.com>
2022-07-11 21:12:38 +0300

    Add translation for helper settings

--- apps/helper/translation.py
+++ apps/helper/translation.py
@@ -1,7 +1,12 @@
 from modeltranslation.translator import register, TranslationOptions
-from .models import FAQ
+from .models import FAQ, HelperSettings
 
 
 @register(FAQ)
 class FAQTranslationOptions(TranslationOptions):
     fields = ('question', 'answer')
+
+
+@register(HelperSettings)
+class HelperSettingsTranslationOptions(TranslationOptions):
+    fields = ('feedback_email_title', 'feedback_email')
