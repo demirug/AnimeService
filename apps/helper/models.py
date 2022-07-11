a4575bd483dc076fb7fd2d5c937a009ef2dae45d
demirug <pm.antondolia@gmail.com>
2022-07-11 21:10:55 +0300

    Created helper settings model

--- apps/helper/models.py
+++ apps/helper/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from solo.models import SingletonModel
 
 
 class FAQ(models.Model):
@@ -30,3 +31,23 @@ class Feedback(models.Model):
 
     def __str__(self):
         return _("Feedback #%s") % self.pk
+
+
+class HelperSettings(SingletonModel):
+
+    """Account settings"""
+    min_feedback_len = models.PositiveIntegerField(help_text=_("Minimum length of feedback text"), default=20)
+    max_feedback_len = models.PositiveIntegerField(help_text=_("Maximum length of feedback text"), default=500)
+    feedback_email_title = models.CharField(_("Title of email"), max_length=50,
+                                               default="Answer on question")
+    feedback_email = models.TextField(_("Feedback email"), default="<h4>Given answer to your question</h4><br><p>Your "
+                                                                   "question:</p><span>"
+                                                                   "{question}</span><br><p>Answer:</p><span>"
+                                                                   "{answer}</span><p>With love AnimeService "
+                                                                   "team</p>")
+
+    def __str__(self):
+        return "Helper Configuration"
+
+    class Meta:
+        verbose_name = "Helper Configuration"
