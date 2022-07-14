ed055e1226b623e7d7c2e96f06906d22a41b7d91
demirug <pm.antondolia@gmail.com>
2022-07-14 18:07:26 +0300

    Removed debug mode from emails

--- shared/services/email.py
+++ shared/services/email.py
@@ -4,16 +4,23 @@ from django.utils.html import strip_tags
 from shared.tasks import send_task_mail
 
 
-def send_email(emails, subject, html_message):
-    """Send to user html email"""
+def send_email(emails, subject: str, html_message: str):
+    """
+    Wrapper to send personal async html email
+
+    :param emails: users emails (can be given single email)
+    :param subject: subject of  letter
+    :param html_message: html messages
+
+    :return: None
+    """
     if isinstance(emails, str):
         emails = [emails]
-    print(html_message)
-    #for email in emails:
-    #    send_task_mail.delay(subject, html_message, strip_tags(html_message), [email])
+    for email in emails:
+        send_task_mail.delay(subject, html_message, strip_tags(html_message), [email])
 
 
-def send_template_email(emails, subject, template, context=None):
+def send_template_email(emails: [], subject: str, template: str, context: dict=None):
     """Send email to user using templates"""
 
     html_message = render_to_string(template, context)
