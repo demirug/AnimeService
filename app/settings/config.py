c19408a53256515e29000143f81f6ff711461465
demirug <pm.antondolia@gmail.com>
2022-07-29 12:12:56 +0300

    Installed dotenv

--- app/settings/config.py
+++ app/settings/config.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+import dotenv
 import jinja2
 from django_jinja.builtins import DEFAULT_EXTENSIONS
 
+
+dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
