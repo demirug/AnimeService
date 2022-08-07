7514f6a0f52f2d5eab18d99236ec00f52c4431a8
demirug <pm.antondolia@gmail.com>
2022-08-08 01:19:24 +0300

    Registered group chat websocket url

--- app/asgi.py
+++ app/asgi.py
@@ -13,7 +13,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-from app.socket_urls import urlpatterns
+from app.websocket_urls import urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
