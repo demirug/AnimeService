6c16160f2cd780b9cb6e540134fde0780b08069d
demirug <pm.antondolia@gmail.com>
2022-08-08 17:24:07 +0300

    Added messages history

--- apps/chat/consumers.py
+++ apps/chat/consumers.py
@@ -4,6 +4,10 @@ from channels.generic.websocket import WebsocketConsumer
 from apps.account.models import User
 from asgiref.sync import async_to_sync
 
+MESSAGES_HISTORY_LENGTH = 20
+
+last_messages = []
+
 
 class ChatConsumer(WebsocketConsumer):
 
@@ -22,6 +26,9 @@ class ChatConsumer(WebsocketConsumer):
             'message': 'You have been connected'
         }))
 
+        for i in range(len(last_messages)):
+            self.send(text_data=last_messages[i])
+
     def receive(self, text_data):
         user: User = self.scope["user"]
         if not user.is_authenticated:
@@ -29,19 +36,24 @@ class ChatConsumer(WebsocketConsumer):
 
         text_data_json = json.loads(text_data)
 
+        message = json.dumps({
+            'type': 'chat',
+            'username': user.username,
+            'avatar': user.get_avatar(),
+            'message': text_data_json["message"]
+        })
+
+        last_messages.append(message)
+        if len(last_messages) > MESSAGES_HISTORY_LENGTH:
+            del last_messages[0]
+
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': json.dumps({
-                    'type': 'chat',
-                    'username': user.username,
-                    'avatar': user.get_avatar(),
-                    'message': text_data_json["message"]
-                })
+                'message': message
             }
         )
 
     def chat_message(self, event):
         self.send(text_data=event['message'])
-
